9d6266f40fcd21d89ab21daa28fea7c91ba85a32
MrMcMuffinsThe3rd <muffin.toughin@gmail.com>
2026-04-11 00:27:44 -0400

    Ok, ACTUALLY got 1 ghost working as intended (minus despawning) Gonna work on spawning more than one now

--- Assets/Scripts/GhostSpawner.cs
+++ Assets/Scripts/GhostSpawner.cs
@@ -5,71 +5,130 @@ using UnityEngine;
 public class GhostSpawner : MonoBehaviour
 {
     public GameObject ghostPrefabs;
-     GameObject spawnedGhosts;
 
-    //GameObject spawnedGhostsPrefabs;
-
-    //public SpriteRenderer ghostSprite;
+    GameObject spawnedGhosts;
 
     public ArrayList spawnedGhostsList;
 
-    public float waitTime = 5;
+    public float waitTime = 3;
     public Vector2 spawnPoint;
 
      public Transform ghostTransform;
+
+    //Coroutine spawnGhostsCoroutine;
+    //Coroutine spawnPositionCoroutine;
+
+    public float t;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //ghostSprite = GetComponent<SpriteRenderer>();
 
-        spawnedGhosts = Instantiate(ghostPrefabs,Vector2.one, Quaternion.identity);
+        //spawnedGhosts = Instantiate(ghostPrefabs,Vector2.one, Quaternion.identity);
 
         //spawnedGhostsPrefabs = spawnedGhosts.GetComponent<GameObject>();
-        ghostTransform = spawnedGhosts.GetComponent<Transform>();
 
-        spawnedGhosts.transform.localScale = ghostTransform.localScale;
 
         //spawnedGhosts = spawnedGhostsPrefabs;
 
-        //transform.position = spawnPoint;
-        //spawnPoint.x = Random.Range(8.5f, -8.5f);
-        //transform.position = spawnPoint;
+
+        StartCoroutine(SpawnGhosts());
+
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        //spawnedGhostsList.Add(spawnedGhosts);
-        StartCoroutine(SpawnGhosts());
+        
 
-        Debug.Log("ghostTransform value:" + ghostTransform);
-        Debug.Log("spawnedGhosts value:" + spawnedGhosts);
+        //spawnedGhostsList.Add(spawnedGhosts);
+
+        //Debug.Log("ghostTransform value:" + ghostTransform);
+        //Debug.Log("spawnedGhosts value:" + spawnedGhosts);
+        //Debug.Log("transform.position.x value:" + spawnPoint.x);
+    }
+
+    public void StartSpawningGhosts()
+    {
+
     }
 
     IEnumerator SpawnGhosts()
     {
-        float t = 0;
+        t = 0;
 
-        while (t < 1)
+        //generates a spawn point based on ghostTransform
+        ghostTransform.position = spawnPoint;
+        spawnPoint.x = Random.Range(8.5f, -8.5f);
+        ghostTransform.position = spawnPoint;
+
+        //spawns ghost prefabs at a random x position (y constant) and no rotation
+        spawnedGhosts = Instantiate(ghostPrefabs, spawnPoint, Quaternion.identity);
+
+        //adds spawned ghosts to a list
+        //spawnedGhostsList.Add(spawnedGhosts);
+
+        Debug.Log("SpawnGhosts Coroutine started");
+
+        while (t < 5)
         {
             t += Time.deltaTime;
-            //simulates ghosts approaching player by increasing their scale
+
+            //make sure spawned ghost's scale is 1 before making them grow (appraoch player)
+            spawnedGhosts.transform.localScale = Vector2.one;
+
+            //gets the transform of the spawned ghosts and assignes it to ghostTransform variable
+            ghostTransform = spawnedGhosts.GetComponent<Transform>();
+
+            //sets the spawned ghosts scale to ghostTransform variable scale
+            spawnedGhosts.transform.localScale = ghostTransform.localScale;
+
+            //simulates ghosts approaching player by increasing their scale (using ghostTransform's scale)
             ghostTransform.localScale = Vector2.one * t;
+
+            //reassign transform.scale to the ghost prefab
+            spawnedGhosts.transform.localScale = ghostTransform.localScale;
+
+            //run this coroutine and check if t=3, if yes, generate a new spawn point
+            //StartCoroutine(SpawnPosition());
+
+            Debug.Log("looping...");
+
+            yield return null; //ghosts will take 10 seconds to reach the player before despawning
+        }
+
+        //despawn the ghost that has reached the player
+
+    }
+
+    IEnumerator SpawnPosition()
+    {
+        Debug.Log("Generating new spawn started");
+
+        while (t == waitTime) //every 3 seconds, generate a new random position and set it to ghostTransform.position then run coroutine that spawns ghosts
+        {
+            ghostTransform.position = spawnPoint;
+            spawnPoint.x = Random.Range(8.5f, -8.5f);
+            ghostTransform.position = spawnPoint;
+
+            //StartCoroutine(SpawnGhosts());
 
             yield return null;
         }
-        
-        ////spawns ghost prefabs at a random x position (y constant) and no rotation
-        //spawnedGhosts = Instantiate(ghostPrefabs,transform.position,Quaternion.identity);
-        ////adds the spawned ghosts game obejcts to the arraylist of spawned ghosts
-        //spawnedGhostsList.Add(spawnedGhosts);
 
-        //yield return new WaitForSeconds(waitTime);
+    }
 
-        //Instantiate(ghostPrefabs, transform.position, Quaternion.identity);
+    IEnumerator StartTheSpawning()
+    {
+        //yield return spawnGhostsCoroutine = StartCoroutine(SpawnGhosts());
 
-        //yield return null;
+        yield return new WaitForSeconds(waitTime);
+
+        //yield return spawnPositionCoroutine = StartCoroutine(SpawnPosition());
+
+        //it's also easier to just do the previous two lines but add yield return new WaitForSeconds() instead of this mess
     }
 }
