0c38398fb144d919a7935cca556ac29f68c52736
MrMcMuffinsThe3rd <muffin.toughin@gmail.com>
2026-04-12 02:57:03 -0400

    got everything to work in invaderSpawner (with multiple peeps) EXCEPT for despawning

--- Assets/Scripts/InvaderSpawner.cs
+++ Assets/Scripts/InvaderSpawner.cs
@@ -11,7 +11,7 @@ public class InvaderSpawner : MonoBehaviour
 
     public List<GameObject> spawnedInvadersList;
 
-    public float waitTime = 5f;
+    public float waitTime = 2f;
     public Vector2 spawnPoint;
 
     public float timer;
@@ -21,10 +21,16 @@ public class InvaderSpawner : MonoBehaviour
     public float t = 0;
     public float speed;
 
+    Coroutine DespawnInvadersCoroutine;
+    Coroutine moveInvadersCoroutine;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        StartCoroutine(SpawnInvaders());
+        pleaseStartIndependentInstancesOfCoroutineIBegU();
+
+        StartCoroutine(StartSpawningInvaders());
+
     }
 
     // Update is called once per frame
@@ -32,23 +38,65 @@ public class InvaderSpawner : MonoBehaviour
     {
         timer += Time.deltaTime;
 
+        //if (moveInvadersCoroutine == null || timer > waitTime) //if it hasnt been assigned a value or its been 2 seconds
+        //{
+        //    startSpawningInvaders(); //spawn another invader 
+        //    timer = 0; //reset the time
+        //}
+
+        //if (DespawnInvadersCoroutine != null && spawnedInvadersList.Count <= 0) //if an invader didnt despawned and there are no invaders in the list
+        //{
+        //    StopCoroutine(DespawnInvaders()); //stop despawning invaders
+        //}
+
+    }
+
+    public void pleaseStartIndependentInstancesOfCoroutineIBegU()
+    {
+        StartCoroutine(MoveInvader());
+    }
+
+    IEnumerator StartSpawningInvaders()
+    {
+
+        timer = 0;
+
+        while (spawnedInvadersList.Count <= 0 || timer > 2)
+        {
+            Debug.Log("spawningInvaders running");
+
+            //generates a spawn point based on invaderTransform
+            invaderTransform.position = spawnPoint;
+            spawnPoint.x = Random.Range(8.5f, -8.5f);
+            spawnPoint.y = 6f;
+            invaderTransform.position = spawnPoint;
+
+            //spawns invader prefabs at a random x position (y constant) and no rotation
+            spawnedInvaders = Instantiate(invaderPrefabs, spawnPoint, Quaternion.identity);
+
+            //adds spawned invaders to a list
+            spawnedInvadersList.Add(spawnedInvaders);
+
+            //gets the transform of the spawned invaders and assignes it to invaderTransform variable
+            invaderTransform = spawnedInvaders.GetComponent<Transform>();
+
+            //sets the spawned invader position to invaderTransform variable position
+            spawnedInvaders.transform.position = invaderTransform.position;
+
+            timer = 0;
+
+            yield return new WaitForSeconds(waitTime);
+        }
+
+        Debug.Log("spawningInvaders done, moving...");
+
+        StartCoroutine(MoveInvader());
+
     }
 
     IEnumerator SpawnInvaders()
     {
-        //generates a spawn point based on invaderTransform
-        invaderTransform.position = spawnPoint;
-        spawnPoint.x = Random.Range(8.5f, -8.5f);
-        spawnPoint.y = 6f;
-        invaderTransform.position = spawnPoint;
 
-        //spawns invader prefabs at a random x position (y constant) and no rotation
-        spawnedInvaders = Instantiate(invaderPrefabs, spawnPoint, Quaternion.identity);
-
-        //adds spawned invaders to a list
-        spawnedInvadersList.Add(spawnedInvaders);
-
-        StartCoroutine(MoveInvader());
 
         yield return null;
     }
@@ -57,45 +105,61 @@ public class InvaderSpawner : MonoBehaviour
     {
         t = 0;
 
-        speed = Random.Range(3, 5f);
-
-        while (t < 4)
+        while (t < 4 || spawnedInvadersList.Count > 0)
         {
-            t += Time.deltaTime;
+            for (int i = spawnedInvadersList.Count - 1; i >= 0; i--)
+            {
+                t += Time.deltaTime;
 
-            //gets the transform of the spawned invaders and assignes it to invaderTransform variable
-            invaderTransform = spawnedInvaders.GetComponent<Transform>();
+                speed = Random.Range(3f, 5f);
 
-            //sets the spawned invader position to invaderTransform variable position
-            spawnedInvaders.transform.position = invaderTransform.position;
+                GameObject moveInvader = spawnedInvadersList[i];
+                Vector2 pos = moveInvader.transform.position;
 
-            //adds speed to y axis of the spawned invader's spawn point to move it downwards
-            spawnPoint.y -= speed * Time.deltaTime; 
+                //adds speed to y axis of the spawned invader's spawn point to move it downwards
+                pos.y -= speed * Time.deltaTime;
 
-            //reassign transform.position to the invader prefab
-            invaderTransform.position = spawnPoint;
+                //reassign transform.position to the invader prefab
+                moveInvader.transform.position = pos;
 
+                invaderTransform.position = pos; //to check position of spawned invaders
 
-            yield return null; //ghosts will take 8 seconds to reach the player before despawning
+                StartCoroutine(DespawnInvaders());
+
+                yield return null; //ghosts will take 8 seconds to reach the player before despawning
+            }
+
+            yield return null;
         }
 
-        new WaitForSeconds(waitTime);
-
-        StartCoroutine(DespawnInvaders());
-        StartCoroutine(SpawnInvaders());
-
-        yield return null;
     }
 
     IEnumerator DespawnInvaders()
     {
-        for (int i = spawnedInvadersList.Count - 1; i >= 0; i--)
+        while (invaderTransform.position.y < -6)
         {
-            GameObject ghost = spawnedInvadersList[i];
-            //despawn the ghost that has reached the player
-            spawnedInvadersList.Remove(ghost);
-            Destroy(ghost);
+            Debug.Log("Despawning....");
+
+            for (int i = spawnedInvadersList.Count - 1; i > 0; i--)
+            {
+                GameObject invader = spawnedInvadersList[i];
+                //despawn the invader that has reached the bottom of the screen
+                spawnedInvadersList.Remove(invader);
+                Destroy(invader);
+
+                yield return null;
+            }
+
+            yield return null;
+
         }
+    }
+
+    IEnumerator MoveAndDespawn()
+    {
+        StartCoroutine(MoveInvader());
+        new WaitForSeconds(waitTime);
+        StartCoroutine(DespawnInvaders());
 
         yield return null;
     }
